b532c84a4d4f0862f6cc3d262d5dfe5575a8ce49
diego <mongaydiego@gmail.com>
2019-02-04 23:55:24 +0100

    nuevas modificaciones para module 1 - solution

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -13,25 +13,24 @@ $scope.countItems = function() {
     if (ListofItems == null) {
       var messageDataFirst = "Please, enter data first";
 
+      $scope.color_message = 'error';
       $scope.message = messageDataFirst;
     } else {
     var arrayOfStrings = ListofItems.split(',');
 
     if (arrayOfStrings.length < 4){
-      var messageEnjoy = "Enjoy!!!";
+      var messageEnjoy = "ENJOY!!!";
 
       $scope.message = messageEnjoy;
+      $scope.color_message = 'success';
 
     } if (arrayOfStrings.length > 3) {
       var messageTooMuch = "Is too much!!!";
 
       $scope.message = messageTooMuch;
+      $scope.color_message = 'error';
 
-    } if (ListofItems == null) {
-      var messageDataFirst = "Please, enter data first";
-
-      $scope.message = messageDataFirst;
-    }
+    } 
     console.log('The array has ' + arrayOfStrings.length + ' Items, separated by a comma (,)');
     }
   }
